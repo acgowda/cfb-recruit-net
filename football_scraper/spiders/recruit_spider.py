8821586cb74c20616f829105c95e5986a0473f2e
Anand Gowda <acgowda@ucla.edu>
2022-12-04 17:13:58 -0800

    Scraped player rankings

--- football_scraper/spiders/recruit_spider.py
+++ football_scraper/spiders/recruit_spider.py
@@ -23,17 +23,23 @@ class RecruitSpider(scrapy.Spider):
             The players that recieved offers from a school, their high schools, and cities. 
         """
         # Select each player on the recruiting list.
-        players = [recruit for recruit in response.css("div.recruit")]
+        players = [recruit for recruit in response.css("div.wrapper")]
         
         regex = r'(.*)\s\((.*),\s(\w+)\)'
 
         for p in players:
             # Extract geodata from text in the format: School (City, State)
-            hs, city, state = re.findall(regex, p.css("span::text").get().strip())[0]
+            hs, city, state = re.findall(regex, p.css("div.recruit span::text").get().strip())[0]
+            
+            try:
+                rank = float(p.css(".score::text").get())
+            except:
+                rank = 0
             
             yield {
                 "name" : p.css("a::text").get().strip(),
                 "school" : hs,
                 "city" : city,
                 "state" : state,
+                "ranking" : rank
                 }
